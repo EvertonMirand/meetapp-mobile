0ba1c3d826a971f76cbaf817e02b68221a9d9af5
Everton Miranda Vitorio <everton.mirandav@gmail.com>
2019-10-16 23:12:56 -0300

    Adiciona servico de Meetup

--- src/services/MeetUpAPI.js
+++ src/services/MeetUpAPI.js
@@ -5,6 +5,21 @@ import api from './api';
 
 const isAndroid = Platform.OS === 'android';
 
+function formatListMeetUps(response) {
+  response.data.map(meetup => {
+    const { File, date } = meetup;
+    if (isAndroid) {
+      if (File) {
+        meetup.File.url = File.url.replace('localhost', '10.0.0.100');
+      }
+    }
+
+    meetup.formattedDate = format(parseISO(date), "dd 'de' MMMM 'às' HH'h'", {
+      locale: pt,
+    });
+  });
+}
+
 export async function loadMeetups(dateParam, page) {
   const response =
     (await api.get('meetups', {
@@ -25,6 +40,32 @@ export async function loadMeetups(dateParam, page) {
     meetup.formattedDate = format(parseISO(date), "dd 'de' MMMM 'às' HH'h'", {
       locale: pt,
     });
+  });
+
+  return response.data;
+}
+
+export async function loadSubscriptions() {
+  const response = (await api.get('subscriptions')) || [];
+
+  response.data.map(subscription => {
+    const { File, date } = subscription.Meetup;
+    if (isAndroid) {
+      if (File) {
+        subscription.Meetup.File.url = File.url.replace(
+          'localhost',
+          '10.0.0.100'
+        );
+      }
+    }
+
+    subscription.Meetup.formattedDate = format(
+      parseISO(date),
+      "dd 'de' MMMM 'às' HH'h'",
+      {
+        locale: pt,
+      }
+    );
   });
 
   return response.data;
