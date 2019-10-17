571f09f7ffa6554c0d50d1c27bd7c382a0d70fcd
Everton Miranda Vitório <c1300290@interno.bb.com.br>
2019-10-17 14:09:21 -0300

    Cria componente FooterIndicator

---
+++ src/components/FooterIndicator/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Indicator } from './styles';
+import Colors from '~/themes/Colors';
+
+export default function FooterIndicator() {
+  return <Indicator color={Colors.defaultIcon} />;
+}
---
+++ src/components/FooterIndicator/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+export const Indicator = styled.ActivityIndicator`
+  margin-top: 10px;
+`;
